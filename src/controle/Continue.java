eefa13c7c2ed64f01669cdbdd33c5e5d861d6ede
enzomanzo <enzomcarelli@gmail.com>
2022-03-05 00:04:23 -0300

    fix

--- src/controle/Continue.java
+++ src/controle/Continue.java
@@ -11,7 +11,7 @@ public class Continue {
             System.out.println("numero par: " + i);
         }
         for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
+            if (i % 2== 0) {
                 continue;
             }
             System.out.println("numero impar: " + i);
