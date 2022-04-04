aa197d462707c96c137f923e7c218b179608a336
Enzo Manzo Carelli <enzomcarelli@gmail.com>
2022-04-04 15:14:51 -0300

    add collection examples

---
+++ src/arrays/Collections/ConjuntoComportado.java
@@ -0,0 +1,33 @@
+package arrays.Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+    public static void main(String[] args) {
+
+        //Set <String> listaAprovados = new HashSet<>();
+        SortedSet <String> listaAprovados = new TreeSet<>(); //imprime em ordem
+        listaAprovados.add("Ana");
+        listaAprovados.add("Carlos");
+        listaAprovados.add("Lucca");
+        listaAprovados.add("Pedro");
+
+        for(String candidato : listaAprovados){
+            System.out.println(candidato);
+        }
+
+        Set<Integer> nums = new HashSet<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        for(int n : nums){
+            System.out.println(n);
+        }
+
+    }
+}
